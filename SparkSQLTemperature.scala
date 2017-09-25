615cce2b60c0d2682647ccbad49d5ca471a0ed94
Shobha <shobhaendigeri@gmail.com>
2017-09-25 20:59:46 +0530

    Add files via upload
    Spark SQL example

---
+++ SparkSQLTemperature.scala
@@ -0,0 +1,45 @@
+package com.sundogsoftware.spark
+
+import org.apache.spark._
+import org.apache.spark.SparkContext._
+import org.apache.spark.sql._
+import org.apache.log4j._
+
+object SparkSQLTemperature {
+case class Temperarure(year:Int,temp:Int)
+
+def mapper(line:String):Temperarure = {
+		val fields = line.split(',')  
+	  val temp:Temperarure = Temperarure(fields(0).toInt,fields(1).toInt)
+		temp
+}
+
+def main(args:Array[String]) {
+	Logger.getLogger("org").setLevel(Level.ERROR)
+
+	val spark = SparkSession
+	.builder
+	.appName("SparkSQL")
+	.master("local[*]")
+	.config("spark.sql.warehouse.dir", "file:///C:/temp")
+	.getOrCreate()
+
+	val lines = spark.sparkContext.textFile("../temperarureFile.txt")
+	val temp = lines.map(mapper)
+
+	import spark.implicits._
+	val schemaPeople = temp.toDS
+
+	schemaPeople.printSchema()
+
+	schemaPeople.createOrReplaceTempView("temperature")
+
+	val teenagers = spark.sql("SELECT * FROM temperature WHERE temp > 50")
+
+	val results = teenagers.collect()
+
+	results.foreach(println)
+
+	spark.stop()
+}
+}
