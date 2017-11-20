63d4fb092f845ea0138b414ae2b78515c733642e
Shobha <shobhaendigeri@gmail.com>
2017-11-20 13:14:01 +0530

    Add files via upload
    FlatMap Example

---
+++ GroupBy.scala
@@ -0,0 +1,15 @@
+package com.spark.examples
+
+import org.apache.spark._
+import org.apache.spark.SparkContext._
+import org.apache.log4j._
+
+object GroupBy {
+  def main(args:Array[String]) {
+    val sc = new SparkContext("local[*]", "GroupBy")
+		val x = sc.parallelize(Array("John","Fred","Anna","James"))
+		val y = x.groupBy(ele => ele.charAt(0))
+		println(y.collect().mkString(", "))
+  }
+  
+}
