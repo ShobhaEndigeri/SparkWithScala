192f2c2311247a8deea9f289643a8b75e5afb49e
Shobha <shobhaendigeri@gmail.com>
2018-04-03 13:33:32 +0530

    Add files via upload

---
+++ structType.scala
@@ -0,0 +1,30 @@
+package stackOverFlow
+
+
+import org.apache.spark._
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+
+object structType {
+	def main(args:Array[String]) {
+		val spark = SparkSession
+				.builder
+				.appName("SparkSQL")
+				.master("local[*]")
+				.config("spark.sql.warehouse.dir", "file:///C:/temp")
+				.getOrCreate()
+
+				import spark.implicits._
+				import org.apache.spark.sql.types.StructType
+
+				val schemaUntyped = new StructType()
+				.add("a", "int")
+				.add("b", "string")
+
+				val schemaUntyped_2 = new StructType()
+				.add($"a".int)
+				.add($"b".string)
+
+	}
+}
