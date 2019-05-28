5866ab10eceeb77047e48f5d59deef3e13a55921
Shobha <shobhaendigeri@gmail.com>
2019-05-28 14:50:36 +0530

    Delete UdfExample.scala

--- UdfExample.scala
+++
@@ -1,8 +0,0 @@
-import spark.implicits._
-case class Stuff(a:String,b:Int)
-val d= sc.parallelize(Seq( ("a",1),("b",2),("",3) ,("d",4)).map { x => Stuff(x._1,x._2)  }).toDF
-
-import org.apache.spark.sql.functions.udf
-
-val func = udf( (s:String) => if(s.isEmpty) 0 else 1 )
-val r = d.select( $"a", $"b", func($"a").as("notempty") )
